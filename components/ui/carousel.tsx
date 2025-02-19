97ec81790be592c955480eaf6c9c3234e5d41e39
Vadim121197 <vadimoleynik039@gmail.com>
2025-02-19 17:31:23 +0200

    fix

--- components/ui/carousel.tsx
+++ components/ui/carousel.tsx
@@ -112,7 +112,7 @@ const Carousel = React.forwardRef<
         carouselRef,
         api: api,
         opts,
-        orientation: orientation || (opts?.axis === 'y' ? 'vertical' : 'horizontal'),
+        orientation: orientation,
         scrollPrev,
         scrollNext,
         canScrollPrev,
